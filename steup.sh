8392c105af74efcadb2e736b8fa2516b52f305fc
DIZHI MA <madizhi@foxmail.com>
2017-10-06 13:25:44 -0700

    spell wrong

--- steup.sh
+++ steup.sh
@@ -3,4 +3,4 @@ cd data
 
 # down load
 Dave Beck [1:14 PM] 
-`curl --output pronto.csv "https://data.seattle.gov/api/views/tw7j-dfaw/rows.csv?accessType=DOWNLOAD"`
+curl --output pronto.csv "https://data.seattle.gov/api/views/tw7j-dfaw/rows.csv?accessType=DOWNLOAD"`
